fc1866c5e84f79b02bb011af0b69730abe7bb65c
Oscar Romero <meru3003@gmail.com>
2022-05-20 20:33:05 +0200

    Created Retrofit adapter to use with coroutines

---
+++ data/src/main/java/cat/oscarromero/randomuser/data/network/adapter/NetworkResponseAdapter.kt
@@ -0,0 +1,18 @@
+package cat.oscarromero.randomuser.data.network.adapter
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Converter
+import java.lang.reflect.Type
+
+class NetworkResponseAdapter<S, E>(
+    private val successType: Type,
+    private val errorBodyConverter: Converter<ResponseBody, E>
+) : CallAdapter<S, Call<NetworkResponse<S, E>>> {
+
+    override fun responseType(): Type = successType
+
+    override fun adapt(call: Call<S>): Call<NetworkResponse<S, E>> =
+        NetworkResponseCall(call, errorBodyConverter)
+}
