cd53327fdb1fdf82edf9b5f7a76b2b61442ad3b9
Oscar Romero <meru3003@gmail.com>
2022-05-22 18:49:04 +0200

    Added ViewModel tests

--- app/src/test/java/cat/oscarromero/randomuser/ui/viewmodel/UsersViewModelTest.kt
+++ app/src/test/java/cat/oscarromero/randomuser/ui/viewmodel/UsersViewModelTest.kt
@@ -34,6 +34,7 @@ class UsersViewModelTest {
         usersViewModel = UsersViewModel(obtainUsers, obtainMoreUsers)
     }
 
+    // region OBTAIN USERS
     @Test
     fun `GIVEN user on users screen, WHEN random users are requested, THEN loading is show`() {
         usersViewModel.loadUsers()
@@ -118,6 +119,93 @@ class UsersViewModelTest {
         Assert.assertNotNull(usersViewModel.failure.value)
     }
 
+    // endregion
+    // region OBTAIN MORE USERS
+    @Test
+    fun `GIVEN user doing scroll, WHEN more random users are requested, THEN loading is show`() {
+        usersViewModel.loadMoreUsers()
+
+        Assert.assertTrue(usersViewModel.isLoading.value!!)
+    }
+
+    @Test
+    fun `GIVEN user doing scroll, WHEN more random users are requested, THEN use case is invoked`() {
+        usersViewModel.loadMoreUsers()
+
+        verify { obtainMoreUsers.invoke(Unit, any()) }
+    }
+
+    @Test
+    fun `GIVEN user doing scroll, WHEN more random users are successful retrieved, THEN loading is hide`() {
+        runMoreUsersSuccessScenario()
+
+        Assert.assertFalse(usersViewModel.isLoading.value!!)
+    }
+
+    @Test
+    fun `GIVEN user doing scroll, WHEN more random users are successful retrieved, THEN model is set`() {
+        val expectedValue = listOf(
+            UserModel(
+                "819-37-3624",
+                "Edwin Freeman",
+                "edwin.freeman@example.com",
+                "https://randomuser.me/api/portraits/men/30.jpg",
+                "(351)-071-1128"
+            ),
+            UserModel(
+                "150287-6138",
+                "Christian Hansen",
+                "christian.hansen@example.com",
+                "https://randomuser.me/api/portraits/men/97.jpg",
+                "98612539",
+            )
+        )
+
+        runMoreUsersSuccessScenario(
+            listOf(
+                User(
+                    "819-37-3624",
+                    "Edwin",
+                    "Freeman",
+                    "edwin.freeman@example.com",
+                    "https://randomuser.me/api/portraits/men/30.jpg",
+                    "(351)-071-1128",
+                    Location("Saddle Dr", "Addison", "Minnesota"),
+                    "male",
+                    SimpleDateFormat("yyyy-MM-dd").parse("2006-01-27")
+                ),
+                User(
+                    "150287-6138",
+                    "Christian",
+                    "Hansen",
+                    "christian.hansen@example.com",
+                    "https://randomuser.me/api/portraits/men/97.jpg",
+                    "98612539",
+                    Location("Harevænget", "Ulsted, Hals", "Syddanmark"),
+                    "male",
+                    SimpleDateFormat("yyyy-MM-dd").parse("2012-10-15")
+                )
+            )
+        )
+
+        Assert.assertEquals(expectedValue, usersViewModel.users.value!!)
+    }
+
+    @Test
+    fun `GIVEN user doing scroll, WHEN more random users fail, THEN loading is hide`() {
+        runMoreUsersFailureScenario()
+
+        Assert.assertFalse(usersViewModel.isLoading.value!!)
+    }
+
+    @Test
+    fun `GIVEN user doing scroll, WHEN random users fail, THEN failure is NOT set`() {
+        runMoreUsersFailureScenario()
+
+        Assert.assertNull(usersViewModel.failure.value)
+    }
+
+    // endregion
     private fun runSuccessScenario(result: List<User> = listOf()) {
         val functionSlot = slot<(Result<List<User>, FailureType>) -> Unit>()
 
@@ -137,4 +225,25 @@ class UsersViewModelTest {
 
         functionSlot.captured.invoke(Result.Failure(FailureType.UnknownFailure()))
     }
+
+    private fun runMoreUsersSuccessScenario(result: List<User> = listOf()) {
+        val functionSlot = slot<(Result<List<User>, FailureType>) -> Unit>()
+
+        usersViewModel.loadMoreUsers()
+
+        verify { obtainMoreUsers.invoke(Unit, capture(functionSlot)) }
+
+        functionSlot.captured.invoke(Result.Success(result))
+    }
+
+    private fun runMoreUsersFailureScenario() {
+        val functionSlot = slot<(Result<List<User>, FailureType>) -> Unit>()
+
+        usersViewModel.loadMoreUsers()
+
+        verify { obtainMoreUsers.invoke(Unit, capture(functionSlot)) }
+
+        functionSlot.captured.invoke(Result.Failure(FailureType.UnknownFailure()))
+    }
+
 }
